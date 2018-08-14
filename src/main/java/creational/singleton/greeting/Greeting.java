bc7fd4024d09011803bf4fb4cbb28a828932e618
binhsonnguyen <son@binhsonnguyen.com>
2018-08-14 16:35:19 +0700

    change static property to be final to avoid renew object later

--- src/main/java/creational/singleton/greeting/Greeting.java
+++ src/main/java/creational/singleton/greeting/Greeting.java
@@ -3,13 +3,13 @@ package creational.singleton.greeting;
 public class Greeting {
 
     // using a static property so not needed to using `new` multiple times
-    private static Greeting theVeryGreatSuperOnlyGreeting = new Greeting();
+    private static final Greeting THE_VERY_GREAT_SUPER_ONLY_GREETING = new Greeting();
 
     private Greeting() {
 
     }
 
     public static Greeting getGreeting() {
-        return theVeryGreatSuperOnlyGreeting;
+        return THE_VERY_GREAT_SUPER_ONLY_GREETING;
     }
 }
