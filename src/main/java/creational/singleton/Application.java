b033fea23b958ed3ee404105f9197c14736a955b
binhsonnguyen <son@binhsonnguyen.com>
2018-08-14 16:31:07 +0700

    using a static property so not needed to using `new` multiple times

--- src/main/java/creational/singleton/Application.java
+++ src/main/java/creational/singleton/Application.java
@@ -3,12 +3,20 @@ package creational.singleton;
 import creational.singleton.greeting.Greeting;
 
 public class Application {
+
+    // using a static property so not needed to using `new` multiple times
+    private static Greeting theVeryGreatSuperOnlyGreeting = new Greeting();
+
     public static void main(String[] args) {
-        Greeting firstGreeting = new Greeting();
-        Greeting secondGreeting = new Greeting();
+        Greeting firstGreeting = getGreeting();
+        Greeting secondGreeting = getGreeting();
 
         // two different object
         System.out.println(firstGreeting);
         System.out.println(secondGreeting);
+    }
+
+    private static Greeting getGreeting() {
+        return theVeryGreatSuperOnlyGreeting;
     }
 }
