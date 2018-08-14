d06c3f235be8c07b3ffa86c11d1507538f53fe65
binhsonnguyen <son@binhsonnguyen.com>
2018-08-14 16:38:58 +0700

    lazing creating singleton object to avoid hardening application's startup

--- src/main/java/creational/singleton/greeting/Greeting.java
+++ src/main/java/creational/singleton/greeting/Greeting.java
@@ -2,14 +2,16 @@ package creational.singleton.greeting;
 
 public class Greeting {
 
-    // using a static property so not needed to using `new` multiple times
-    private static final Greeting THE_VERY_GREAT_SUPER_ONLY_GREETING = new Greeting();
+    private static Greeting theVeryGreatSuperOnlyGreeting;
 
     private Greeting() {
 
     }
 
     public static Greeting getGreeting() {
-        return THE_VERY_GREAT_SUPER_ONLY_GREETING;
+        if (theVeryGreatSuperOnlyGreeting == null) {
+            theVeryGreatSuperOnlyGreeting = new Greeting();
+        }
+        return theVeryGreatSuperOnlyGreeting;
     }
 }
